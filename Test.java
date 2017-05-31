0ab3cb9fcb8b27109af5aae5035dff52eaf0b484
abhimanyura <abhimanyu.ise@gmail.com>
2017-05-31 12:40:00 +0530

    Second change

--- Test.java
+++ Test.java
@@ -4,6 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("intial");
+System.out.println("change1");
 	}
-
+public void add(int a,int b )
+{
+	System.out.println("add");
+}
 }
